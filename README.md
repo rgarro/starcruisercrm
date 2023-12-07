53264bffc9b46fc4ecf3091485a9bd2589dd4066
rgarro <rgarro@gmail.com>
2023-12-06 21:42:52 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# starcruisercrm
---
+++ starcruisercrm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  starcruisercrm
+//
+//  Created by Rolando Garro on 12/6/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
